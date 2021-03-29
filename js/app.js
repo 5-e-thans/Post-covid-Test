a352dea352824f153bec0d77a0208392895bb6a0
CullenSharp2 <cullen.sharp100@gmail.com>
2021-03-29 13:40:52 -0700

    Add todo comments

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 'use strict'
-
+// TODO: fill with answers
 const shows = [];
 const actors = [];
 
 const mainElem = document.getElementsByTagName('main');
 
 
-
-function Person (name, show, img) {
+//constructor for all the objects
+function Person(name, show, img) {
     this.name = name;
     this.show = show;
     this.img = img;
     Person.all.push(this);
 }
 
+// holds all instances of the Person object
 const answerKey = Person.all[];
 
 new Person('Cillian Murphy', 'Peaky Blinders', 'img/CM Peaky Blinders.jpg');
@@ -27,20 +28,23 @@ new Person('Eugene Levy', "Schitt's Creek", 'imgs/eugene-levyschitts-creek.jpg')
 new Person('William Zabika', 'Cobra Kai', 'imgs/Cobra.jpg');
 new Person('Phoebe Dynevor', 'Bridgerton', 'imgs/Phoebe Dynevor Bridgerton.jpeg');
 
-
+// TODO: add images to the screen
 function renderImages() {
     const mainElem.src = Person.img;
     const mainElem.textContent = `This is actor in show`
 }
 
+// TODO: hit submit and move to the next question
 function cycleQuestions() {
 
     for (let i = 0; i < array.length; i++) {
-    renderImages();        
+        renderImages();
     }
 }
 
+// TODO: add an event listener to submit
 
+// TODO: create handler for submit
 
 
 
@@ -48,9 +52,9 @@ function cycleQuestions() {
 //This will be the shuffle function used to randomize inncorrect answers.
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * i);
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+        const j = Math.floor(Math.random() * i);
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
-  }
+}
