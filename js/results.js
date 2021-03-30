0f1c3b92d794bdcf77c2d18afde795b7f7d5f423
CullenSharp2 <cullen.sharp100@gmail.com>
2021-03-30 15:13:08 -0700

    Debug debug handleAnswer and getScore

--- js/results.js
+++ js/results.js
@@ -1,11 +1,15 @@
 'use strict'
 
 function getScore() {
+    const results = document.getElementById('finalScore');
     let finalScore = localStorage.getItem('scores');
+    
     if (finalScore !== null) {
         let parsedScore = JSON.parse(finalScore);
         console.log(parsedScore);
+        
+        results.textContent = `Your final score is ${parsedScore}`;
     }
 }
-const results = document.getElementById('finalScore');
-results.textContent = `Your final score is ${parsedScore}`;
+
+getScore();
